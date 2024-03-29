f86ac00e0cfebb325f42ca9baa8beb05a65900d6
monster <monster@192.168.1.11>
2024-03-29 17:22:31 +0530

    added file upload

---
+++ src/Controller/Component/FileUploadComponent.php
@@ -0,0 +1,23 @@
+// File: Controller/Component/FileUploadComponent.php
+<?php
+namespace App\Controller\Component;
+
+use Cake\Controller\Component;
+use Cake\Utility\Text;
+
+class FileUploadComponent extends Component {
+
+    public function upload($fileData, $destinationFolder) {
+        $filename = Text::uuid() . '-' . $fileData['name'];
+        $destination = WWW_ROOT . $destinationFolder . DS . $filename;
+
+        if (move_uploaded_file($fileData['tmp_name'], $destination)) {
+            return $filename;
+        } else {
+            return false;
+        }
+    }
+}
+
+
+?>
